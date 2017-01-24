09abb49fc462aca749e4db93756ef9d66db7c5ab
kevinchenghua <s9822108@m98.nthu.edu.tw>
2017-01-24 10:31:56 +0800

    Add the gridlstm layer computation graph of tensorflow
    Use the nested scan to compute the output of Grid3LSTMCell.

---
+++ hwr/gridlstm.py
@@ -0,0 +1,160 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow.contrib import grid_rnn
+
+
+class GridLSTMLayer():
+    def __init__(self):
+        pass
+
+
+def _scan_grid(cell, input, raw_states_init, init_col_states):
+    """Function for scaning grid
+
+    Note:
+        cell_memory_size = cell_hidden_size = cell_output_size = num_units
+        cell_state_size = cell_memory_size + cell_hidden_size = 2 * num_units
+        state_size = 2 * cell_state_size (from up and left)
+        output_size = cell_output_size
+    Args:
+        cell: grid LSTM cell
+        input: input Tensor, 4D, num_raws x num_cols x batch_size x input_size
+        raw_states_init: raw_states initializer Tensor, 3D,
+                         num_cols x batch_size x cell_state_size
+        init_col_states: column_states initializer Tensor, 3D,
+                         num_raws x batch_size x cell_state_size
+    Returns:
+        output: output Tensor, 4D,
+                num_raws x num_cols x batch_size x output_size
+    """
+    def loop(init, elem):
+        """Loop function for scan
+
+        Args:
+            elem: (raw_input, init_col_states_elem)
+                raw_input: 3D Tensor of raw_input element,
+                           num_cols x batch_size x input_size
+                init_col_states_elem: column state initializer Tensor, 2D,
+                                      batch_size x cell_state_size
+            init: (raw_output, raw_states_init)
+                raw_output: 3D Tensor of raw_output,
+                            num_cols x batch_size x output_size
+                raw_states_init: raw_states initializer Tensor, 3D,
+                                 num_cols x batch_size x cell_state_size
+        Returns: (output_elem, raw_elem, col_init)
+            raw_output: 3D Tensor of raw_output,
+                        num_cols x batch_size x output_size
+            raw_states: raw_states Tensor, 3D,
+                        num_cols x batch_size x cell_state_size
+        """
+
+        # build the computation graph of the raw
+        raw_output, raw_states = _scan_grid_raw(
+            cell, elem[0], init[1], elem[1])
+
+        return raw_output, raw_states
+
+    # prepare the initializer tensors for the scan function
+    raw_states_shape = tf.shape(raw_states_init)
+    raw_output_shape = [raw_states_shape[0],
+                        raw_states_shape[1],
+                        raw_states_shape[2] // 2]
+    init_raw_output = tf.zeros(raw_output_shape)
+
+    # scan the grid
+    output, _ = tf.scan(
+        loop,
+        elems=(input, init_col_states),
+        initializer=(init_raw_output, raw_states_init))
+
+    return output
+
+
+def _scan_grid_raw(cell, raw_input, prev_raw_states, init_col_state):
+    """Function for scaning raw of grid
+
+    Note:
+        cell_memory_size = cell_hidden_size = cell_output_size = num_units
+        cell_state_size = cell_memory_size + cell_hidden_size = 2 * num_units
+        state_size = 2 * cell_state_size (from up and left)
+        output_size = cell_output_size
+    Args:
+        cell: grid LSTM cell
+        raw_input: raw_input Tensor, 3D, num_cols x batch_size x input_size
+        prev_raw_states: prev raw Tensor, 3D,
+                         num_cols x batch_size x cell_state_size
+        init_col_state: column state initializer Tensor, 2D,
+                         batch_size x cell_state_size
+    Returns:
+        raw_output: raw_output Tensor, 3D, num_cols x batch_size x output_size
+        raw_states: raw_state Tensor, 3D,
+                    num_cols x batch_size x cell_state_size
+    """
+    def loop(init, elem):
+        """Loop function for scan
+
+        Args:
+            elem: (raw_elem_input, prev_raw_states_elem)
+                raw_input_elem: 2D Tensor of raw_input element,
+                             batch_size x input_size
+                prev_raw_states_elem: 2D Tensor of prev_raw_states element,
+                               batch_size x cell_state_size
+            init: (raw_output_elem, raw_states_elem, init_col_state)
+                raw_output_elem: 2D Tensor of raw_output element,
+                                 batch_size x output_size
+                raw_states_elem: 2D Tensor of raw_states element,
+                          batch_size x cell_state_size
+                init_col_state: column state initializer Tensor, 2D,
+                          batch_size x cell_state_size
+        Returns: (raw_output_elem, raw_states_elem, init_col_state)
+            raw_output_elem: 2D Tensor of raw_output element,
+                             batch_size x output_size
+            raw_states_elem: 2D Tensor of raw_states element,
+                             batch_size x cell_state_size
+            init_col_state: 2D Tensor, batch_size x cell_state_size
+        """
+
+        # prepare for the inputs of gridlstm cell
+        cell_input = elem[0]
+        cell_states = tf.concat(1, [init[2], elem[1]])
+
+        # build the computation graph of the cell
+        raw_output_elem, states = cell(cell_input, cell_states)
+
+        # split the cell ouput `states` to `col_state` and `raw_state`
+        init_col_state, raw_states_elem = tf.split(1, 2, states)
+
+        return raw_output_elem, raw_states_elem, init_col_state
+
+    # prepare the initializer tensors for the scan function
+    raw_states_elem_shape = tf.shape(init_col_state)
+    raw_output_elem_shape = [raw_states_elem_shape[0],
+                             raw_states_elem_shape[1] // 2]
+    init_raw_states_elem = tf.zeros(raw_states_elem_shape)
+    init_raw_output_elem = tf.zeros(raw_output_elem_shape)
+
+    # scan the raw
+    output, raw_states, _ = tf.scan(
+        loop,
+        elems=(raw_input, prev_raw_states),
+        initializer=(init_raw_output_elem,
+                     init_raw_states_elem,
+                     init_col_state))
+
+    return output, raw_states
+
+
+def test():
+    input = tf.zeros([100, 150, 1, 3])
+    raw = tf.zeros([150, 1, 100])
+    col = tf.zeros([100, 1, 100])
+    gl = grid_rnn.Grid3LSTMCell(50, non_recurrent_fn=tf.nn.relu)
+    # The initializer is needed
+    with  tf.variable_scope("test", initializer=tf.random_normal_initializer()):
+        output = _scan_grid(gl, input, raw, col)
+
+
+test()
